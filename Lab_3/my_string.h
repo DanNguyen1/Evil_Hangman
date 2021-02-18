65ef9c144968837106c18ee8bd37409b4ff0e19b
DanNguyen1 <Dan_Nguyen1@student.uml.edu>
2021-02-18 12:10:14 -0500

    separated status.h from my_string.h

--- Lab_3/my_string.h
+++ Lab_3/my_string.h
@@ -1,10 +1,7 @@
 #ifndef MY_STRING_H
 #define MY_STRING_H
-#ifndef STATUS_H
-#define STATUS_H
+#include "status.h"
 typedef void * MY_STRING;
-enum status{FAILURE, SUCCESS};
-typedef enum status Status;
 
 MY_STRING my_string_init_default(void);
 MY_STRING my_string_init_c_string(const char* c_string);
@@ -22,4 +19,3 @@ Status my_string_insertion(MY_STRING hMy_string, FILE* fp);
 void my_string_destroy(MY_STRING* phMy_string);
 
 #endif//MY_STRING_H
-#endif//STATUS_H
