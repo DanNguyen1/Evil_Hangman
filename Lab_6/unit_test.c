4a6ca89b68ef7a2a957c365d6b9284505893c65a
DanNguyen1 <Dan_Nguyen1@student.uml.edu>
2021-03-04 13:22:16 -0500

    start lab 6

---
+++ Lab_6/unit_test.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "unit_test.h"
+int main(void)
+{
+	Status (*tests[])(char*, unsigned long) =
+	{
+		test_init_default_returns_nonNULL,
+		test_get_size_on_init_default_returns_0,
+		test_get_capacity_on_init_default_returns_7,
+		test_init_c_string_helloworld_returns_nonNULL,
+		test_get_size_on_init_c_string_helloworld_returns_10,
+		test_get_capacity_on_init_c_string_helloworld_returns_11,
+		test_compare_two_strings_returns_0,
+		test_compare_two_strings_returns_less_than_0,
+		test_compare_two_strings_returns_greater_than_0,
+		test_destroy_does_not_leave_dangling_pointer,
+		test_my_string_extraction_successfully_extracts,
+		test_my_string_insertion_successfully_inserts,
+		test_my_string_extraction_extracts_correct_word,
+		test_my_string_push_back_returns_SUCCESS,
+		test_my_string_push_back_updates_capacity,
+		test_my_string_push_back_copies_old_string_and_adds_new_char,
+		test_my_string_pop_back_returns_SUCCESS,
+		test_my_string_pop_back_returns_FAILURE_for_empty_string,
+		test_my_string_pop_back_decreases_size_by_1,
+		test_my_string_pop_back_has_correct_string_after_pop,
+		test_my_string_at_returns_correct_char_from_string,
+		test_my_string_c_str_returns_correct_c_string,
+		test_my_string_concat_returns_correct_concat_c_string,
+		test_my_string_concat_returns_correct_size_after_concat,
+		test_my_string_empty_returns_TRUE_for_empty_string
+	};
+	int number_of_functions = sizeof(tests) / sizeof(tests[0]);
+	int i;
+	char buffer[500];
+	int success_count = 0;
+	int failure_count = 0;
+	
+	for(i=0; i<number_of_functions; i++)
+	{
+		if(tests[i](buffer, 500) == FAILURE)
+		{
+			printf("FAILED: Test %d failed miserably\n", i);
+			printf("\t%s\n", buffer);
+			failure_count++;
+		}
+		else
+		{
+			// printf("PASS: Test %d passed\n", i);
+			// printf("\t%s\n", buffer);
+			success_count++;
+		}
+	}
+	printf("Total number of tests: %d\n", number_of_functions);
+	printf("%d/%d Pass, %d/%d Failure\n", success_count,
+	number_of_functions, failure_count, number_of_functions);
+
+	return 0;
+}
---
+++ Lab_6/unit_test.h
@@ -0,0 +1,31 @@
+#ifndef UNIT_TEST_H
+#define UNIT_TEST_H
+#include "my_string.h"
+
+Status test_init_default_returns_nonNULL(char* buffer, unsigned long length);
+Status test_get_size_on_init_default_returns_0(char* buffer, unsigned long length);
+Status test_get_capacity_on_init_default_returns_7(char* buffer, unsigned long length);
+Status test_init_c_string_helloworld_returns_nonNULL(char* buffer, unsigned long length);
+Status test_get_size_on_init_c_string_helloworld_returns_10(char* buffer, unsigned long length);
+Status test_get_capacity_on_init_c_string_helloworld_returns_11(char* buffer, unsigned long length);
+Status test_compare_two_strings_returns_0(char* buffer, unsigned long length);
+Status test_compare_two_strings_returns_less_than_0(char* buffer, unsigned long length);
+Status test_compare_two_strings_returns_greater_than_0(char* buffer, unsigned long length);
+Status test_destroy_does_not_leave_dangling_pointer(char* buffer, unsigned long length);
+Status test_my_string_extraction_successfully_extracts(char* buffer, unsigned long length);
+Status test_my_string_insertion_successfully_inserts(char* buffer, unsigned long length);
+Status test_my_string_extraction_extracts_correct_word(char* buffer, unsigned long length);
+Status test_my_string_push_back_returns_SUCCESS(char* buffer, unsigned long length);
+Status test_my_string_push_back_updates_capacity(char* buffer, unsigned long length);
+Status test_my_string_push_back_copies_old_string_and_adds_new_char(char* buffer, unsigned long length);
+Status test_my_string_pop_back_returns_SUCCESS(char* buffer, unsigned long length);
+Status test_my_string_pop_back_returns_FAILURE_for_empty_string(char* buffer, unsigned long length);
+Status test_my_string_pop_back_decreases_size_by_1(char* buffer, unsigned long length);
+Status test_my_string_pop_back_has_correct_string_after_pop(char* buffer, unsigned long length);
+Status test_my_string_at_returns_correct_char_from_string(char* buffer, unsigned long length);
+Status test_my_string_c_str_returns_correct_c_string(char* buffer, unsigned long length);
+Status test_my_string_concat_returns_correct_concat_c_string(char* buffer, unsigned long length);
+Status test_my_string_concat_returns_correct_size_after_concat(char* buffer, unsigned long length);
+Status test_my_string_empty_returns_TRUE_for_empty_string(char* buffer, unsigned long length);
+
+#endif
