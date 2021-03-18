fc2569191f57a09e26c6c79475deb529d76b263b
DanNguyen1 <Dan_Nguyen1@student.uml.edu>
2021-03-18 14:11:30 -0400

    added testing in main

--- Evil_Hangman/main.c
+++ Evil_Hangman/main.c
@@ -24,7 +24,34 @@ int main(void)
 	while(my_string_extraction(hString, dict_txt))
 	{
 		generic_vector_push_back(dictionary[my_string_get_size(hString)], hString);
-	}	
+	}
+
+	printf("word size is 0\n"
+			"\texpected 0 words and got %d words\n", generic_vector_get_size(dictionary[0]));
+        printf("word size is 1\n"
+                        "\texpected 0 words and got %d words\n", generic_vector_get_size(dictionary[1]));
+        printf("word size is 2\n"
+                        "\texpected 94 words and got %d words\n", generic_vector_get_size(dictionary[2]));
+	printf("\tThe 0 word is %s\n", my_string_c_str(generic_vector_at(dictionary[2], 0)));
+	printf("\tThe 93 word is %s\n", my_string_c_str(generic_vector_at(dictionary[2], 93)));
+        printf("word size is 3\n"
+                        "\texpected 962 words and got %d words\n", generic_vector_get_size(dictionary[3]));
+	printf("\tThe 0 word is %s\n", my_string_c_str(generic_vector_at(dictionary[3], 0)));
+	printf("\tThe 961 word is %s\n", my_string_c_str(generic_vector_at(dictionary[3], 961)));
+        printf("word size is 4\n"
+                        "\texpected 3862 words and got %d words\n", generic_vector_get_size(dictionary[4]));
+	printf("\tThe 0 word is %s\n", my_string_c_str(generic_vector_at(dictionary[4], 0)));
+        printf("\tThe 3861 word is %s\n", my_string_c_str(generic_vector_at(dictionary[4], 3861)));
+        printf("word size is 5\n"
+                        "\texpected 8548 words and got %d words\n", generic_vector_get_size(dictionary[5]));
+	printf("\tThe 0 word is %s\n", my_string_c_str(generic_vector_at(dictionary[5], 0)));
+        printf("\tThe 8547 word is %s\n", my_string_c_str(generic_vector_at(dictionary[5], 8547)));
+        printf("word size is 6\n"
+                        "\texpected 14383 words and got %d words\n", generic_vector_get_size(dictionary[6]));
+	printf("\tThe 0 word is %s\n", my_string_c_str(generic_vector_at(dictionary[6], 0)));
+        printf("\tThe 14382 word is %s\n", my_string_c_str(generic_vector_at(dictionary[6], 14382)));
+
+
 
 	for(i = 0; i < MAX_WORD_SIZE; ++i)
 	{
