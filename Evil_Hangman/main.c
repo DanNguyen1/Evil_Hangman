0cef161a1efd06bf82750d987a0e27153e90061e
DanNguyen1 <Dan_Nguyen1@student.uml.edu>
2021-04-03 00:51:27 -0400

    Evil Hangman finished, works as intended - could be polished

--- Evil_Hangman/main.c
+++ Evil_Hangman/main.c
@@ -13,6 +13,7 @@ int get_num_guesses(void);
 int get_guess(MY_STRING guessed_letters, char *guess, GENERIC_VECTOR current_family);
 Boolean get_running_total(void);
 Status generate_first_key(MY_STRING key, int word_length);
+void input(MY_STRING read);
 void print_guessed_letters(MY_STRING guessed_letters);
 void clear_keyboard_buffer(void);
 
@@ -30,6 +31,7 @@ int main(void)
 	int i;
 	int word_length;
 	int num_guesses;
+	int check;
 	Boolean running_total;
 	char play_again;
 	char guess;
@@ -91,40 +93,67 @@ int main(void)
 			printf("Word: %s\n", my_string_c_str(current_key));
 
 			printf("Enter guess: ");
-			get_guess(guessed_letters, &guess, current_family);
-			clear_keyboard_buffer();
+			check = get_guess(guessed_letters, &guess, current_family);
 			
-			for(i = 0; i < generic_vector_get_size(current_family); ++i)
+			if (check == 1)
 			{
-				get_word_key_value(current_key, new_key, generic_vector_at(current_family, i), guess);
-				tree_insert(&root, generic_vector_at(current_family, i), new_key);
+				for(i = 0; i < generic_vector_get_size(current_family); ++i)
+				{
+					get_word_key_value(current_key, new_key, generic_vector_at(current_family, i), guess);
+					tree_insert(&root, generic_vector_at(current_family, i), new_key);
+				}
+
+				if (running_total)
+				{
+					print_inorder_traversal(root);
+				}
+
+				new_word_family = get_largest_family(root);
+
+				if (my_string_compare(current_key, get_key(new_word_family)) == 0)
+				{
+					printf("I'm sorry, there were no %c's in this word.\n", guess);
+					num_guesses--;
+				}
+
+				generic_vector_assignment(&current_family, get_word_family(new_word_family));
+				my_string_assignment(&current_key, get_key(new_word_family));
+			
+				if (running_total)
+				{
+					printf("The computer has %d possibilities remaining.\n", generic_vector_get_size(current_family));
+				}
+
+				tree_destroy(&root);
 			}
 
-			if (running_total)
+			else if (check == 2)
 			{
-				print_inorder_traversal(root);
+				printf("You have already guessed every letter.\n");
+				if (running_total)
+				{
+					printf("The computer has %d possibilities remaning. \n", generic_vector_get_size(current_family));
+				}
 			}
-
-			new_word_family = get_largest_family(root);
-
-			if (my_string_compare(current_key, get_key(new_word_family)) == 0)
+			
+			else if (check < 0)
 			{
-				printf("I'm sorry, there were no %c's in this word.\n", guess);
+				printf("Sorry, that word is incorrect.\n");
 				num_guesses--;
 			}
-
-			generic_vector_assignment(&current_family, get_word_family(new_word_family));
-			my_string_assignment(&current_key, get_key(new_word_family));
-			
-			if (running_total)
+			else if (check == 0)
 			{
-				printf("The computer has %d possibilities remaining.\n", generic_vector_get_size(current_family));
+				printf("That is the correct word!\n");
+				break;
 			}
-
-			tree_destroy(&root);
 
 			printf("\n");
 
+		}
+
+		if (num_guesses > 0)
+		{
+			printf("Congratulations!\n");
 		}
 
 		generic_vector_destroy(&current_family); //might cause problems, does not check for every element........
@@ -132,6 +161,8 @@ int main(void)
 		my_string_destroy(&current_key);
 
 		my_string_destroy(&new_key);
+
+		my_string_destroy(&guessed_letters);
 		
 		//play again prompt
 		printf("Would you like to play again? (y\\n)\n");
@@ -192,18 +223,10 @@ int get_guess(MY_STRING guessed_letters, char *guess, GENERIC_VECTOR current_fam
 	MY_STRING compare = NULL;
 	int i;
 	char ch;
+	
+	input(read);
 
-	do
-       	{
-		scanf("%c", &ch);
-		if (ch == '\n') {
-			break;
-		}
-		my_string_push_back(read, ch);
-
-	} while (ch == '\n');
-
-	if (my_string_get_size(read) == 1)
+	if (my_string_get_size(read) == 1 && my_string_get_size(guessed_letters) < 26)
 	{
 		i = 0;
 		while(i < my_string_get_size(guessed_letters))
@@ -212,7 +235,7 @@ int get_guess(MY_STRING guessed_letters, char *guess, GENERIC_VECTOR current_fam
 			{	
 				printf("You have already guessed that letter. Please enter another letter:\n");
 				my_string_pop_back(read);
-				scanf("%c", &ch);
+                		scanf(" %c", &ch);
 				my_string_push_back(read, ch);
 				i = 0;
 			}
@@ -226,6 +249,12 @@ int get_guess(MY_STRING guessed_letters, char *guess, GENERIC_VECTOR current_fam
 		*guess = *(my_string_at(read, 0));
 		my_string_destroy(&read);
 		return 1;
+	}
+
+	else if (my_string_get_size(read) == 1 && my_string_get_size(guessed_letters) >= 26)
+	{
+		return 2;
+
 	}
 
 	else
@@ -287,6 +316,20 @@ Status generate_first_key(MY_STRING key, int word_length)
 	}
 
 	return SUCCESS;
+}
+
+void input(MY_STRING read)
+{
+	char ch;
+
+	do
+        {
+                scanf("%c", &ch);
+                if (ch == '\n') {
+                        break;
+                }
+                my_string_push_back(read, ch);
+        } while (ch != '\n');
 }
 
 void print_guessed_letters(MY_STRING guessed_letters)
